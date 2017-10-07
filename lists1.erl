9a7982a08bbf42fe7e8791241f2345480e6ca087
Didans <dawid.ns@hotmail.com>
2017-10-07 10:59:35 +0200

    Commit message

---
+++ lists1.erl
@@ -0,0 +1,21 @@
+-module(lists1).
+-compile([export_all]).
+
+len([]) -> 0;
+len([_|L]) -> 1+len(L).
+
+amin([H|T]) -> amin(H, T).
+amin(M, [H|T]) when M < H -> amin(M, T);
+amin(M, [H|T]) when M >= H -> amin(H, T);
+amin(M, []) ->  M.
+
+amax([H|T]) -> amax(H, T).
+amax(M, [H|T]) when M > H -> amax(M, T);
+amax(M, [H|T]) when M =< H -> amax(H, T);
+amax(M, []) -> M.
+
+tmin_max([H|T]) -> {amin([H|T]), amax([H|T])}.
+lmin_max([H|T]) -> [amin([H|T]), amax([H|T])].
+
+mklist(1) -> [1];
+mklist(N) -> [N|mklist(N-1)].
