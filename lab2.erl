b9d98405e0147fa3c4e3cc13640e11a5667d4fd5
Didans <dawid.ns@hotmail.com>
2017-10-15 12:18:25 +0200

    V2.0

--- lab2.erl
+++ lab2.erl
@@ -11,13 +11,22 @@ map_display(Map) -> maps:to_list(Map).
 %czytaj po lini dla zwiekszenia efektywnosci
 %%%%%%%%%%%%%%%%%%%%
 
-fromLtoM([]) -> [];
-fromLtoM([H|T]) -> [{H,1}|fromLtoM(T)].
+listToMap([],Map) -> Map;
+listToMap([H|T],Map) ->
+	case maps:is_key(H,Map) of
+		true ->
+			Value = maps:get(H, Map),
+			Map1 = maps:update(H, Value+1, Map);
+		false ->
+			Map1 = maps:put(H, 1, Map)
+	end,
+	listToMap(T,Map1).
+
 
 readfile(FileName) ->
 	{ok, Binary} = file:read_file(FileName),
 	Lines = string:tokens(erlang:binary_to_list(Binary), "\n .,"),
-	maps:from_list(fromLtoM(Lines)).
-	
+	Map = maps:new(),
+	listToMap(Lines,Map).
 
 
