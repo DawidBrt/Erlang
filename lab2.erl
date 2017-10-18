9e2850a7cda5a2bd23e3856ed7f89fe0a8f50e13
Didans <dawid.ns@hotmail.com>
2017-10-18 16:59:22 +0200

    Added wildcard.

--- lab2.erl
+++ lab2.erl
@@ -7,40 +7,64 @@ map_update(Key, Value, Map) -> maps:update(Key, Value, Map).
 
 map_display(Map) -> maps:to_list(Map).
 
-%f-cja wildcard
-%%%%%%%%%%%%%%%%%%%%
+map_get(Key, Map) ->
+	Klow = string:to_lower(Key),
+	maps:get(Klow, Map).
+
+%%%%%%%%%%%%%%%
 
 listToMap([],Map) -> Map;
 listToMap([H|T],Map) ->
-	case maps:is_key(H,Map) of
+	Hlow = string:to_lower(H),
+	case maps:is_key(Hlow,Map) of
 		true ->
-			Value = maps:get(H, Map),
-			Map1 = maps:update(H, Value+1, Map);
+			Value = maps:get(Hlow, Map),
+			Map1 = maps:update(Hlow, Value+1, Map);
 		false ->
-			Map1 = maps:put(H, 1, Map)
+			Map1 = maps:put(Hlow, 1, Map)
 	end,
 	listToMap(T,Map1).
+
+
+%%%%%%%%%%% Basic one %%%%%%%%%%%%%%%%
+readWholeFile(FileName) ->
+	{ok, Binary} = file:read_file(FileName),
+	Lines = string:tokens(erlang:binary_to_list(Binary), "\n .,"),
+	Map = maps:new(),
+	listToMap(Lines,Map).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 readFile(FileName) ->
 	{ok, File} = file:open(FileName, read),
 	Map = maps:new(),
 	readByLine(File,Map).
 
+readFile(FileName, Map) ->
+	{ok, File} = file:open(FileName, read),
+	readByLine(File,Map).
+
 readByLine(File, Map) ->
     case file:read_line(File) of
-        {ok, Data} -> Line = string:tokens(Data, "\n .,"),
+        {ok, Data} -> Line = string:tokens(Data, "\n .,[{()}]\\/"),
 					Map1 = listToMap(Line,Map),
 					readByLine(File,Map1);
         eof        -> Map;
 		{error, Reason} -> Reason
     end.
 
-readWholeFile(FileName) ->
-	{ok, Binary} = file:read_file(FileName),
-	Lines = string:tokens(erlang:binary_to_list(Binary), "\n .,"),
-	Map = maps:new(),
-	listToMap(Lines,Map).
+%%%%%%%%%%%WILD%%%%%%%%%%%%%%%%%%
+wildList([],Map) -> Map;
+wildList([H|T], Map) -> 
+	Map1 = readFile(H, Map),
+	wildList(T,Map1).
 
+wildTxt() ->
+	L = filelib:wildcard("*.txt"),	%you need txt files in directory
+	Map = maps:new(),
+	MapEnd = wildList(L, Map),
+	MapEnd.
+	
+	
 
 
 
